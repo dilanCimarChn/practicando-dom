0d04533e990b1f005c06fa6ecfbe51ba4004fae9
dilanCimarChn <2002dilanchoque@gmail.com>
2023-11-29 23:35:16 -0400

    ya hecho la prueba la parte de dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
+// App.jsx
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import './App.css'; // Archivo CSS para las transiciones
 import Header from './components/Header';
 import Home from './components/Home';
 import About from './components/About';
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,16 @@
+// Header.jsx
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <header>
       <h1>My App</h1>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/dashboard">Dashboard</Link>
+      </nav>
     </header>
   );
 };
